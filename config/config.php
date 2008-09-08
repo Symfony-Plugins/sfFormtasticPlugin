63c5ae30db12007148e6b8dd2764e43d84781ef1
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-08 16:14:49 -0500

    sfFormtasticPlugin: Moved registration of config handler back into yaml.
    git-svn-id: http://svn.symfony-project.com/plugins/sfFormtasticPlugin/trunk@11381 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,9 +1,4 @@
 <?php
 
-// register sfFormtasticYamlConfigHandler and sfFormtasticYamlAutoload to 
-// enable autoloading of YAML forms
-
-$this->getConfigCache()->registerConfigHandler('form/*.yml', 'sfFormtasticYamlConfigHandler');
-
 $autoload = sfFormtasticYamlAutoload::getInstance(sfConfig::get('sf_config_cache_dir').'/sfFormtasticYamlAutoloadCache.txt');
 $autoload->register();
